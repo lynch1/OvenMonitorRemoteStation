fb313f8eb81aed70abe700d65b72893ffbcb6f9d
lynch1 <dlynch02132@gmail.com>
2019-04-18 21:18:41 -0400

    Updated sources and instructions

--- remotemqttclient.py
+++ remotemqttclient.py
@@ -7,20 +7,3 @@ MQTT_REMOTE_STATION_NAME = "BlueMOven"
 N0_FEEDS = 1
 FEED_1_NAME = "ThermistorReading"
 FEED_1_VALUE = "number"
-
-# def MQTT_BROKER_IP():
-# 	return "18.218.123.57"
-# def MQTT_BROKER_PORT():
-# 	return 1883
-# def USER_NAME():
-# 	return "dan"
-# def USER_PSWD():
-# 	return "stella"
-# def MQTT_REMOTE_STATION_NAME():
-# 	return "BlueMOven"
-# def N0_FEEDS(): 
-# 	return 1
-# def FEED_1_NAME():
-# 	return "ThermistorReading"
-# def FEED_1_VALUE():
-# 	return "number"
