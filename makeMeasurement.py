03b969c44de6675abed931ff889858113a741a9d
lynch1 <dlynch02132@gmail.com>
2019-05-02 20:00:02 -0400

    Changed the feeds to the serial feeds and combined the makeMeasurement and serialTest

--- makeMeasurement.py
+++ makeMeasurement.py
@@ -8,23 +8,30 @@
 import time
 import datetime
 import random
+import serial
 import remotemqttclient as config
 
 measurementFile = open(config.LATEST_MEASUREMENTS, 'w')
 
 sampleTime = datetime.datetime.now()
 
-sumValue = 0
-# Main program loop.
-for i in range(10):
-    sumValue = sumValue + random.randrange(10)
-    time.sleep(0.5)
 
-sumValue = sumValue / 10
+# Open Serial Connection
+sp = serial.Serial(port = "/dev/ttyUSB0", baudrate = 57600, bytesize = serial.EIGHTBITS, parity = serial.PARITY_NONE, stopbits = serial.STOPBITS_ONE, timeout = 1)
+qmsg  = bytearray("?:3010:00::c2\r", "ascii")
+sp.write(qmsg)
+line = sp.readline()
+# Nice to see what's happening
+# print(line)
+# Parse the recieved message
+setTemp = line[9:17]
+currentTemp = line[17:24]
+
+tempTemp = setTemp + ", " currentTemp
 
 messageString = config.REMOTE_STATION_NAME + '~' + config.FEED_1_NAME + '~'
 messageString = messageString + str(sampleTime) + '~'
-messageString = messageString + str(sumValue)
+messageString = messageString + str(tempTemp)
 
 measurementFile.write(messageString)
 
--- remotemqttclient.py
+++ remotemqttclient.py
@@ -3,14 +3,14 @@ MQTT_BROKER_IP = "18.218.123.57"
 MQTT_BROKER_PORT =  1883
 USER_NAME = "dan"
 USER_PSWD = "stella"
-REMOTE_STATION_NAME = "BlueMOven"
+REMOTE_STATION_NAME = "Heratherm"
 MQTT_MESSAGE_TOPIC = "OvenTemp"
 LATEST_MEASUREMENTS = "/home/pi/OvenMonitorRemoteStation/latest-measurements.txt"
 
 N0_FEEDS = 5
 
-FEED_1_NAME = "Thermo1"
-FEED_2_NAME = "Thermo1"
-FEED_3_NAME = "Thermo1"
-FEED_4_NAME = "Thermo1"
-FEED_5_NAME = "Thermo1"
+FEED_1_NAME = "Temps"
+FEED_2_NAME = "Unused"
+FEED_3_NAME = "Unused"
+FEED_4_NAME = "Unused"
+FEED_5_NAME = "Unused"
