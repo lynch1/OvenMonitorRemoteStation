1d760d1631f1d64f9d709bef38c01df3122ec37d
lynch1 <dlynch02132@gmail.com>
2019-05-30 17:54:45 -0400

    Update remotemqttclient.py

--- remotemqttclient.py
+++ remotemqttclient.py
@@ -1,8 +1,8 @@
 
-MQTT_BROKER_IP = "18.218.123.57"
-MQTT_BROKER_PORT =  1883
-USER_NAME = "dan"
-USER_PSWD = "stella"
+MQTT_BROKER_IP = "xxx.xxx.xxx.xxx"
+MQTT_BROKER_PORT =  xxxx
+USER_NAME = "xxxxxxxx"
+USER_PSWD = "xxxxxxxx"
 REMOTE_STATION_NAME = "Heratherm"
 MQTT_MESSAGE_TOPIC = "OvenTemp"
 LATEST_MEASUREMENTS = "/home/pi/OvenMonitorRemoteStation/latest-measurements.txt"
